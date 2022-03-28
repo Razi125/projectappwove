6a92e301405f811ca4c74399c82ec24b80d16c4d
lf-devs <devs@learnforce.app>
2022-03-28 18:27:59 +0500

    wove project update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Main from '../Components/Layout/Main'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
